0e4f3cab8ea51a845d47e2f8ffb97ffc55ae67e5
Imanm02 <imanm1381@gmail.com>
2023-02-23 18:55:21 +0330

    fixing auth parts

--- accounts/backends.py
+++ accounts/backends.py
@@ -22,25 +22,3 @@ class EmailAuthenticationBackend(BaseBackend):
             return user_model.objects.get(pk=user_id)
         except user_model.DoesNotExist:
             return None
-
-
-class PhoneNumberAuthenticationBackend(BaseBackend):
-    """
-    Custom Email Backend to perform authentication via phone number
-    """
-
-    def authenticate(self, username=None, password=None):
-        user_model = get_user_model()
-        try:
-            user = user_model.objects.get(phone_number=username, is_active=True)
-            if user.check_password(password):
-                return user
-        except user_model.DoesNotExist:
-            return None
-
-    def get_user(self, user_id):
-        user_model = get_user_model()
-        try:
-            return user_model.objects.get(pk=user_id)
-        except user_model.DoesNotExist:
-            return None
