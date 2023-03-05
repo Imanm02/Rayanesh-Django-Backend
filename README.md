33fbbcd7716e12c9304a95a45d0b4f640344727d
Imanm02 <imanm1381@gmail.com>
2023-03-06 00:53:59 +0330

    update readme

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Rayanesh Site Back-end
 
 * use "run.sh" commands to run
+
+## python manage.py makemigrations
+## python manage.py migrate
+## python manage.py runserver
--- RayaneshBackend/urls.py
+++ RayaneshBackend/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('v1/podcast/', include('podcast.urls')),
     path('v1/gallery/', include('gallery.urls')),
     path('v1/staff/', include('staff.urls')),
+    path('v1/accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
