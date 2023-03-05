502f9736f32fb07d4fbec71385bd270d903b5c4d
Imanm02 <imanm1381@gmail.com>
2023-03-05 20:54:38 +0330

    edit settings

--- RayaneshBackend/settings.py
+++ RayaneshBackend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +50,6 @@ INSTALLED_APPS = [
     'blog',
     'gallery',
     'staff',
-    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -169,11 +169,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'accounts:profile'
 LOGIN_URL = 'login'
