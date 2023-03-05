5f8546e542e4fbf3dacbd8c8cb9f3e7124048faf
Imanm02 <imanm1381@gmail.com>
2023-03-05 20:52:04 +0330

    add auth

--- RayaneshBackend/settings.py
+++ RayaneshBackend/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'blog',
     'gallery',
     'staff',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -165,3 +166,17 @@ CELERY_TASK_SERIALIZER = 'json'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = 'accounts:profile'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
