38d343625ee1714be7c7bd53974d55eb69c8d192
Imanm02 <imanm1381@gmail.com>
2023-03-05 06:07:02 +0330

    update depracate code

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from .views import BlogPostList, BlogPostDetail, BlogPostCreate
 urlpatterns = [
     path('', BlogPostList.as_view(), name='blogposts'),
     path('<int:pk>/', BlogPostDetail.as_view(), name='blogpost'),
-    path('create/', BlogPostCreate, name='blogpost'),
+    path('create/', BlogPostCreate.as_view(), name='blogpost'),
 ]
