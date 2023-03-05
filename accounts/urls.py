1fc9f83d47f982f27d26031572d09b5a75166603
Imanm02 <imanm1381@gmail.com>
2023-03-05 21:05:26 +0330

    include accounts url

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,18 +9,13 @@ from .forms import UserLoginForm, PwdResetForm, PwdChangeForm, PwdResetForm, Pwd
 app_name = 'accounts'
 
 urlpatterns = [
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name="registration/password_change_form.html",
-                                                                   form_class=PwdChangeForm), name='pwdforgot'),
-    path('login/', auth_views.LoginView.as_view(template_name="registration/login.html",
-                                                authentication_form=UserLoginForm), name='login'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html",
-                                                                 form_class=PwdResetForm), name='pwdreset'),
-    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
-        template_name='registration/password_reset_confirm.html', form_class=PwdResetConfirmForm), name="pwdresetconfirm"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='pwdforgot'),
+    path('login/', auth_views.LoginView.as_view(authentication_form=UserLoginForm), name='login'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(form_class=PwdResetForm), name='pwdreset'),
+    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(form_class=PwdResetConfirmForm), name="pwdresetconfirm"),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.edit, name='edit'),
     path('profile/delete/', views.delete_user, name='deleteuser'),
     path('register/', views.accounts_register, name='register'),
-    path('activate/<slug:uidb64>/<slug:token>)/',
-         views.activate, name='activate'),
+    path('activate/<slug:uidb64>/<slug:token>)/', views.activate, name='activate'),
 ]
