6d93428fed855d472c4a7f455a207045df625cc5
hasti <hastikarimi2002@gmail.com>
2023-03-09 22:04:08 +0330

    blog migration fixed

--- RayaneshBackend/urls.py
+++ RayaneshBackend/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('v1/podcast/', include('podcast.urls')),
     path('v1/gallery/', include('gallery.urls')),
     path('v1/staff/', include('staff.urls')),
+    # path('v1/users/', include('accounts.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
